0c572b54d43b094529bae5549e2fb9e2eaf50c6a
kibum414 <kibum414@naver.com>
2021-03-24 15:18:40 +0900

    210324

--- Spring/workspace/workspace (Spring - React, Gradle)/be/demo/src/main/java/com/example/demo/uss/controller/UserController.java
+++ Spring/workspace/workspace (Spring - React, Gradle)/be/demo/src/main/java/com/example/demo/uss/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.example.demo.uss.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.uss.domain.UserDto;
+import com.example.demo.uss.domain.User;
+import com.example.demo.uss.service.UserServiceImpl;
 
-@Controller
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
 public class UserController {
 	
-	@Autowired UserDto user;
+	private final UserServiceImpl service;
 	
-	public void test() {
-		String a = user.getPassword();
+	@PostMapping("/insert")
+	public ResponseEntity<User> save(@RequestBody User user) {
+		service.save(user);
+		
+		return null;
 	}
 }
