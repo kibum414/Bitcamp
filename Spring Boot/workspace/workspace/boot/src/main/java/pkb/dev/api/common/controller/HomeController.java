8b4d20d9ab2aa67a52fdb3e9d27967f4cc780f60
kibum414 <kibum414@naver.com>
2021-04-14 14:27:52 +0900

    210414

---
+++ Spring Boot/workspace/workspace/boot/src/main/java/pkb/dev/api/common/controller/HomeController.java
@@ -0,0 +1,4 @@
+package pkb.dev.api.common.controller;
+
+public class HomeController {
+}
---
+++ Spring Boot/workspace/workspace/boot/src/test/java/pkb/dev/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pkb.dev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
