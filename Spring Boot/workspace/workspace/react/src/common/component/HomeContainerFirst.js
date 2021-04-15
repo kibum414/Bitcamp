62efde87eebeec25bb0de6d80bb5cbedaa931ae0
kibum414 <kibum414@naver.com>
2021-04-15 16:40:24 +0900

    210415

---
+++ Spring Boot/workspace/workspace/react/src/common/component/HomeContainerFirst.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomeContainerFirst = () => {
+
+  return (
+    <>
+      <div className="container-fluid bg-1 text-center">
+        <h3 className="margin">Who Am I?</h3>
+        <img src="https://www.w3schools.com/bootstrap/bird.jpg" className="img-responsive img-circle margin display_inline" alt="Bird" width="350" height="350" />
+        <h3>I'm an adventurer</h3>
+      </div>
+    </>
+  )
+}
+
+export default HomeContainerFirst
