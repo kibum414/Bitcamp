e97b2f3f51f657601626f0dee41691c84f7b3a61
kibum414 <kibum414@naver.com>
2021-04-30 02:44:39 +0900

    Optional

--- Spring Boot/React/Final/boot/src/main/java/kb/dev/api/participant/service/ParticipantServiceImpl.java
+++ Spring Boot/React/Final/boot/src/main/java/kb/dev/api/participant/service/ParticipantServiceImpl.java
@@ -1,7 +1,54 @@
 package kb.dev.api.participant.service;
 
+import kb.dev.api.common.service.AbstractService;
+import kb.dev.api.participant.domain.Participant;
+import kb.dev.api.participant.repository.ParticipantRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class ParticipantServiceImpl {
+@RequiredArgsConstructor
+public class ParticipantServiceImpl extends AbstractService<Participant> implements ParticipantService {
+
+    private final ParticipantRepository repository;
+
+    @Override
+    public long count() {
+        return repository.count();
+    }
+
+    @Override
+    public boolean existsById(long id) {
+        return repository.existsById(id);
+    }
+
+    @Override
+    public List<Participant> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Participant> findById(long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Optional<Participant> getOne(long id) {
+        return Optional.of(repository.getOne(id));
+    }
+
+    @Override
+    public long save(Participant participant) {
+        return (repository.save(participant) != null) ? 1L : 0L;
+    }
+
+    @Override
+    public long delete(Participant participant) {
+        repository.delete(participant);
+
+        return (findById(participant.getParticipantNo()).orElse(null) == null) ? 1L : 0L;
+    }
 }
