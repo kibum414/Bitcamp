8a81cda0482e254052d57f6aee35a04a540e0346
kibum414 <kibum414@naver.com>
2021-05-03 22:46:25 +0900

    Authority

--- Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/domain/Role.java
+++ Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/domain/Role.java
@@ -19,7 +19,7 @@ public enum Role implements GrantedAuthority {
     public static Role of(String code) {
         // enum은 배열 : 유저에 따라 Role의 개수가 다르기 때문에 배열화
         // ADMIN 이나 USER 라는 Role이 있으면 넣고, 없으면 UNKNOWN
-        // 후에 람다 등에 활용할 경우 stream 화 필요
+        // 후에 람다 등에 활용할 경우 stream 객체여야 하기 때문에 stream화
         return Arrays
                 .stream(Role.values())
                 .filter(i -> i.getCode().equals(code))
