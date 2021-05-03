8547088b82999daf76663161efc435757ad09508
kibum414 <kibum414@naver.com>
2021-05-03 14:22:37 +0900

    Spring Security

--- Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/controller/UserController.java
+++ Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package kb.dev.api.user.controller;
 
 import kb.dev.api.user.domain.UserVo;
+import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -27,6 +29,27 @@ public class UserController {
 
     @DeleteMapping("")
     public ResponseEntity<Long> delete(@RequestBody UserVo userVo) {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserVo>> all() {
+        log.info("로그인 하지 않은 사용자도 접근 가능한 URI");
+
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/{username}")
+    public ResponseEntity<?> auth(@PathVariable String username) {
+        log.info("로그인 한 사용자만 접근 가능한 URI");
+
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/admin")
+    public ResponseEntity<?> admin() {
+        log.info("관리자만 접근 가능한 URI");
+
         return ResponseEntity.ok(null);
     }
 }
