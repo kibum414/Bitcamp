7fcd4a4555792ed0d7a34a21a2eb0bdc90da8cd9
kibum414 <kibum414@naver.com>
2021-05-04 22:17:37 +0900

    Spring Security

--- Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/service/UserDetailsServiceImpl.java
+++ Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/service/UserDetailsServiceImpl.java
@@ -3,7 +3,9 @@ package kb.dev.api.user.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Override // 아이디가 맞으면 유저를 로딩시키는 서비스 하나만 존재
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
