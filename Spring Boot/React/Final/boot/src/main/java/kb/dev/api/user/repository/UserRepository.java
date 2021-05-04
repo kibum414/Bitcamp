4e65910ffd4248f2573caf59dc62f967e60703ae
kibum414 <kibum414@naver.com>
2021-05-04 22:15:49 +0900

    Spring Security

--- Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/repository/UserRepository.java
+++ Spring Boot/React/Final/boot/src/main/java/kb/dev/api/user/repository/UserRepository.java
@@ -2,6 +2,13 @@ package kb.dev.api.user.repository;
 
 import kb.dev.api.user.domain.UserVo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserVo, Long> {
+    boolean existsByUsername(String username);
+    UserVo findByUsername(String username);
+
+    @Query (value = "SELECT * FROM users WHERE username = :username AND password = :password", nativeQuery = true)
+    UserVo signin(@Param("username") String username, @Param("password") String password);
 }
