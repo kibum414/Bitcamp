36174c4a0cd0d06816eb245a056b44717321bd56
kibum414 <kibum414@naver.com>
2021-05-05 01:12:12 +0900

    Spring Security - Filter

--- Spring Boot/React/Final/boot/src/main/java/kb/dev/api/security/config/SecurityConfig.java
+++ Spring Boot/React/Final/boot/src/main/java/kb/dev/api/security/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package kb.dev.api.security.config;
 
-import org.springframework.context.annotation.Configuration;
+import kb.dev.api.security.aop.SecurityFilter;
+import kb.dev.api.security.domain.SecurityProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
+// 필터
+@RequiredArgsConstructor
 public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final SecurityProvider provider;
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        SecurityFilter filter = new SecurityFilter(provider);
+        // Controller Request 들어오기 전에 얘를 찾아라
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
