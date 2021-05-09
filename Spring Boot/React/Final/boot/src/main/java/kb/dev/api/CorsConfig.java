e59338c21ec9762851556fa3a535becd81275105
kibum414 <kibum414@naver.com>
2021-05-09 13:38:32 +0900

    CORS

--- Spring Boot/React/Final/boot/src/main/java/kb/dev/api/CorsConfig.java
+++ Spring Boot/React/Final/boot/src/main/java/kb/dev/api/CorsConfig.java
@@ -1,5 +1,16 @@
 package kb.dev.api;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3000);
+    }
 }
