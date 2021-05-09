f460257da50b31ae51f1253a47f25866cac32da1
kibum414 <kibum414@naver.com>
2021-05-09 13:48:37 +0900

    RTK 회원가입

--- Spring Boot/React/Final/react/src/user/index.js
+++ Spring Boot/React/Final/react/src/user/index.js
@@ -1,5 +1,9 @@
+// 속성
 export { default as Login } from 'user/component/Login'
 export { default as SignUp } from 'user/component/SignUp'
 export { default as UserDetail } from 'user/component/UserDetail'
 export { default as UserEdit } from 'user/container/UserEdit'
-export { default as UserList } from 'user/container/UserList'
+export { default as UserApp } from 'user/container/UserApp'
+
+// 기능
+export { default as UserService } from 'user/service/user.service'
